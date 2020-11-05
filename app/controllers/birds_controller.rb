ca6fc2ab2ab3280a50c0dd8c9031d1ff06ff4937
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-05 12:21:11 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-    render json: { birds: birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    render json: birds.to_json(except: [:created_at, :updated_at])
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: {bird: bird.slice(:id, :name, :species), message: "Yo, check out this bird"}
+    else
+      render json: {message: 'Bird not found'}
+    end
   end
 end
